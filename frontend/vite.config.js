f32217d17869511d722808efa0d74763da9654e2
Lokkuchakreshkumar <lokkuchakreshkumar@gmail.com>
2025-07-11 21:00:45 +0530

    added an vite pwa

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,34 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
+import iamge from "../frontend/public/"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(),tailwindcss(),
+      VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: "KLU Desk – Your College Companion",
+        short_name: "KLU Desk",
+        start_url: ".",
+        display: "standalone",
+        background_color: "#000000",
+        theme_color: "#000000",
+        icons: [
+          {
+            src: "../frontend/public/KLUDESKICON-v2.png",
+            type: "image/png",
+            sizes: "192x192"
+          },
+          {
+            src: "../frontend/public/KLUDESKICON.png",
+            type: "image/png",
+            sizes: "512x512"
+          }
+        ]
+      }
+    })
+  ],
 })
