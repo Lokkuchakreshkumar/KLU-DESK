d31fe0b80e0341a0b7d37eb143731e3d4aa407a0
Lokkuchakreshkumar <lokkuchakreshkumar@gmail.com>
2025-09-03 15:06:52 +0530

    added Talk to dev button

--- frontend/src/Components/Maintenance.jsx
+++ frontend/src/Components/Maintenance.jsx
@@ -9,6 +9,10 @@ const Maintenance = () => {
             Not downtime. Just me hand-coding pain into progress. Expect new madness soon.
         </div>
         <video className='sm:w-[40%] my-4 w-[90%]' src={video} autoPlay muted loop></video>
+
+            <a  target='_blank' href="https://wa.me/918688913488?text=Hey%20Chakresh%2C%20we%20want%20to%20contribute%20to%20KLU%20DESK
+
+" className='p-4 border border-white text-white rounded-lg hover:bg-cyan hover:text-black hover:bg-cyan-400 hover:scale-105 hover:shadow-lg hover:shadow-cyan-500/50'>Talk to Developer</a>
     </div>
   )
 }
