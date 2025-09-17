9eada42159e5b24d260acb49069b75b3714e7fb1
Lokkuchakreshkumar <lokkuchakreshkumar@gmail.com>
2025-09-17 12:09:38 +0530

    added apeksha grp button

--- frontend/src/Components/Credits/Join.jsx
+++ frontend/src/Components/Credits/Join.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { FaWhatsapp } from "react-icons/fa";
+import apeksha from '../../../public/Adobe Express - file (4).png'
 
 const Join = () => {
   return (
@@ -12,6 +13,9 @@ const Join = () => {
 " className='p-4 border border-black text-white rounded-lg hover:bg-cyan hover:text-black hover:bg-amber-400 hover:scale-105 hover:shadow-lg  hover:shadow-amber-500/50'>Contribute</a>
 <a target='_blank' href="https://chat.whatsapp.com/GSfZoEOlE809OSQFQj2j0c?mode=r_t" className='border  border-green-500 hover:shadow-lg hover:shadow-green-500/50 text-green-500  p-4 ml-2 bg-transparent hover:rounded-xl hover:bg-green-500 hover:text-white'><FaWhatsapp className='inline mr-4 text-green-500s'/>Join our Community</a>
         </div>
+        <a href='https://t.me/+5BUV-tlZKmViMmE9' className='p-2 py-3 mt-4 flex hover:bg-black gap-x-8 hover:text-amber-400 hover:shadow-xl hover:shadow-black hover:cursor-pointer  sm:w-[35%] w-[90%] items-center rounded-xl justify-center text-amber-500  border border-amber-400'>
+           <img src={apeksha} className='sm:w-[10%] w-[20%]' alt="" /><span>Join Y25 Apeksha </span>
+        </a>
 
     </div>
   )
