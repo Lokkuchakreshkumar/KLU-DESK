0695f903f178374058832a931ba97abc42295d0f
Lokkuchakreshkumar <lokkuchakreshkumar@gmail.com>
2026-01-29 22:51:42 +0530

    change numbers

--- frontend/src/Components/Intro_card2.jsx
+++ frontend/src/Components/Intro_card2.jsx
@@ -4,7 +4,7 @@ export default function Intro_card2(){
         <div className="flex justify-center border-4 mt-4 rounded-3xl px-8 py-4 border-amber-300 bg-[#121212] items-center">
             <div><IoPeople className="inine mr-2 text-amber-800 text-lg"/></div>
            <div className="flex ml-2 flex-col items-center">
-            <div className="text-amber-600 font-semibold">7200+</div>
+            <div className="text-amber-600 font-semibold">7910+</div>
             <div className="text-white/50">Students</div>
            </div>
         </div>
